99218cd5709e833ef075ca275123da93952f3ebe
Roderick-Agnes <m.contactqc@gmail.com>
2022-11-30 16:18:01 +0700

    fix responsive mobile detail page

--- src/components/Breadcrumb/index.jsx
+++ src/components/Breadcrumb/index.jsx
@@ -1,6 +1,7 @@
 import { useState,useEffect } from "react";
 import { MdArrowForwardIos } from "react-icons/md";
 import { Link } from "react-router-dom";
+import {sliceTitleShort} from "../../utils/sliceTitleShort"
 
 const Breadcrumb = ({name, category}) => {
   const [breadcrumb, setBreadcrumb] = useState()
@@ -14,14 +15,15 @@ const Breadcrumb = ({name, category}) => {
   
   return (
     <div className="flex items-center max-h-[40px] h-[40px] w-full  mobile:pl-4">
-      <ul className="list-none flex flex-row  gap-1 ">
+      <ul className="list-none flex flex-row  gap-1">
         {breadcrumb && breadcrumb.map((item, idx) => {
           return (
             <li
               key={`breadscrumb-title-${idx}`}
               className="flex items-center gap-1 text-[#808089] text-[14px]"
             >
-              {item?.link ? <Link to={item?.link} className="text-[#808089] text-[14px] hover:underline cursor-pointer">{item?.name}</Link> : item?.name}
+              {item?.link ? <Link to={item?.link} className="text-[#808089] text-[14px] hover:underline cursor-pointer">{item?.name}</Link> : (<><Link className=" text-[#808089] text-[14px] tablet:hidden">{sliceTitleShort(item?.name, 15)}</Link>
+              <Link className=" text-[#808089] text-[14px] x-mobile:hidden mobile:hidden">{item?.name}</Link></>)}
               
               {idx < breadcrumb.length - 1 && (
                 <MdArrowForwardIos className="text-[#808089] text-[14px]" />
